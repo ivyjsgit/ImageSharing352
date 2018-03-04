6c1bdb6cf64a5fb327b8474b7922361b9a5cc786
Jonathan Ivy <ivyjs@hendrix.edu>
2018-03-04 14:41:01 -0600

    Externalized defaultDirectory

--- src/main/java/ImageShareGUI/ImageShareController.java
+++ src/main/java/ImageShareGUI/ImageShareController.java
@@ -1,6 +1,5 @@
 package ImageShareGUI;
 
-import Networking.BasicSharing;
 import Networking.RequestAllImages;
 import ReusableClasses.Images.SharableImage;
 import ReusableClasses.Users.User;
@@ -17,162 +16,125 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+import static Storage.DefaultDirectory.readDefaultDirectory;
+import static Storage.DefaultDirectory.saveDirectory;
 
 public class ImageShareController {
 
-	@FXML
-	Label ipViewer;
+    @FXML
+    Label ipViewer;
 
-	@FXML
-	TextField otherIP;
+    @FXML
+    TextField otherIP;
 
-	@FXML
-	Button upload;
+    @FXML
+    Button upload;
 
-	@FXML
-	Button request;
+    @FXML
+    Button request;
 
-	@FXML
-	ImageView imageDown;
+    @FXML
+    ImageView imageDown;
 
-	@FXML
-	TilePane tiles;
+    @FXML
+    TilePane tiles;
 
-	FileChooser fileChooser = new FileChooser();
-	User shareUser = new User("", "");
-	File directory = new File("Directory.txt");
-	String defaultDir;
+    FileChooser fileChooser = new FileChooser();
+    User shareUser = new User("", "");
+    File directory = new File("Directory.txt");
+    String defaultDir;
 
-	public void initialize() {
-		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("*.jpg", "*.jpg"),
-				new FileChooser.ExtensionFilter("*.jpg", "*.jpeg"), new FileChooser.ExtensionFilter("*.png", "*.png"));
-		if (!directory.exists()) {
-			DirectoryChooser dirChoose = new DirectoryChooser();
-			dirChoose.setTitle("Choose initial directory");
-			File chosenDir = dirChoose.showDialog(new Stage());
-			defaultDir = chosenDir.getAbsolutePath();
-			saveDirectory(defaultDir);
-		} else {
-			/*
-			 * Found at
-			 * http://javarevisited.blogspot.com/2015/09/how-to-read-file-into-
-			 * string-in-java-7.html#ixzz58o7FRLqr
+    public void initialize() {
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("*.jpg", "*.jpg"),
+                new FileChooser.ExtensionFilter("*.jpg", "*.jpeg"), new FileChooser.ExtensionFilter("*.png", "*.png"));
+        if (!directory.exists()) {
+            DirectoryChooser dirChoose = new DirectoryChooser();
+            dirChoose.setTitle("Choose initial directory");
+            File chosenDir = dirChoose.showDialog(new Stage());
+            defaultDir = chosenDir.getAbsolutePath();
+            saveDirectory(defaultDir, directory);
+        } else {
+			/* 
+			Found at http://javarevisited.blogspot.com/2015/09/how-to-read-file-into-string-in-java-7.html#ixzz58o7FRLqr
 			 */
-			try {
-				InputStream is = new FileInputStream(directory);
-				BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+            readDefaultDirectory(directory, defaultDir);
+        }
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String ip = InetAddress.getLocalHost().getHostAddress();
+                    System.out.println(ip);
+                    shareUser.setIP(ip);
+                    ipViewer.setText("Your IP is: " + ip);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
 
-				String line = buf.readLine();
-				StringBuilder sb = new StringBuilder();
+            }
+        });
 
-				while (line != null) {
-					sb.append(line).append("\n");
-					line = buf.readLine();
-				}
+    }
 
-				defaultDir = sb.toString();
-				buf.close();
-			} catch (Exception e) {
-				e.getMessage();
-			}
-		}
-		Platform.runLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					String ip = InetAddress.getLocalHost().getHostAddress();
-					System.out.println(ip);
-					shareUser.setIP(ip);
-					ipViewer.setText("Your IP is: " + ip);
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-				}
 
-			}
-		});
+    public void uploadImage() {
+        fileChooser.setTitle("Select Image");
+        File chosenFile = fileChooser.showOpenDialog(new Stage());
 
-	}
+        new Thread(() -> {
 
-	private void saveDirectory(String absolutePath) {
-		try {
-			Scanner fileIn = new Scanner(absolutePath);
-			PrintStream fileOut = new PrintStream(directory);
+            SharableImage chosenImage = new SharableImage(chosenFile, chosenFile.getName(), "test");
+            shareUser.addSharbleImage(chosenImage);
+            System.out.println("Crashing?");
 
-			while (fileIn.hasNextLine()) {
-				fileOut.print(fileIn.nextLine());
-			}
+            Image image = SwingFXUtils.toFXImage(chosenImage.getImage().get(), null);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    ImageView upImage = new ImageView(image);
+                    tiles.getChildren().add(upImage);
 
-			fileIn.close();
-			fileOut.close();
-		} catch (FileNotFoundException e) {
-			e.getMessage();
-		}
-	}
+                }
 
-	public void uploadImage() {
-		fileChooser.setTitle("Select Image");
-		File chosenFile = fileChooser.showOpenDialog(new Stage());
+            });
 
-			new Thread(() -> {
+            try {
+                RequestAllImages.receiveAllImages(shareUser.getFiles());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            ;
+        }, "uploadingThread").start();
 
-			SharableImage chosenImage = new SharableImage(chosenFile, chosenFile.getName(), "test");
-			shareUser.addSharbleImage(chosenImage);
-			System.out.println("Crashing?");
+    }
 
-			Image image = SwingFXUtils.toFXImage(chosenImage.getImage().get(), null);
-			Platform.runLater(new Runnable() {
-				@Override
-				public void run() {
-					ImageView upImage = new ImageView(image);
-					tiles.getChildren().add(upImage);
+    public void requestImage() throws IOException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<SharableImage> receivedImages = new ArrayList<SharableImage>();
+                System.out.println("Getting image!");
+                try {
+                    System.out.println(otherIP.getText());
+                    receivedImages.addAll(RequestAllImages.requestAllImages(otherIP.getText()));
 
-				}
-
-			});
-
-			try {
-				RequestAllImages.receiveAllImages(shareUser.getFiles());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			;
-		},"uploadingThread").start();
-
-	}
-
-	public void requestImage() throws IOException {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				ArrayList<SharableImage> receivedImages = new ArrayList<SharableImage>();
-				System.out.println("Getting image!");
-				try {
-					System.out.println(otherIP.getText());
-					receivedImages.addAll(RequestAllImages.requestAllImages(otherIP.getText()));
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				for (SharableImage x : receivedImages) {
-					ImageView tileImage;
-					Image image = SwingFXUtils.toFXImage(x.getImage().get(), null);
-					tileImage = new ImageView(image);
-					tiles.getChildren().addAll(tileImage);
-				}
-			}
-		}).start();
-	}
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                for (SharableImage x : receivedImages) {
+                    ImageView tileImage;
+                    Image image = SwingFXUtils.toFXImage(x.getImage().get(), null);
+                    tileImage = new ImageView(image);
+                    tiles.getChildren().addAll(tileImage);
+                }
+            }
+        }).start();
+    }
 }
